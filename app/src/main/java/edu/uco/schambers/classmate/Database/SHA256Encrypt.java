200a83af02a1c4a368c7031ff0fdf3de4d1c602e
nelalison <calitova@gmail.com>
2015-09-13 19:01:06 -0500

    added space to SHA256Encrypt

--- app/src/main/java/edu/uco/schambers/classmate/Database/SHA256Encrypt.java
+++ app/src/main/java/edu/uco/schambers/classmate/Database/SHA256Encrypt.java
@@ -12,6 +12,7 @@ import java.security.NoSuchAlgorithmException;
 public class SHA256Encrypt {
 
     public static String encrypt(String data) {
+        
         try {
             MessageDigest md = MessageDigest.getInstance("SHA-256");
             md.update(data.getBytes("UTF-8")); // Change this to "UTF-16" if needed
